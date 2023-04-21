f9a3f6ff96392775eaa58f924b92bfc5ca185563
Sunny kumar <98548654+Sunnykumar17@users.noreply.github.com>
2023-04-21 16:02:42 +0530

    Add files via upload

---
+++ src/components/Comp3.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Comp3.css";
+const Comp3 = () => {
+  return (
+    <div style={{margin:"10px"}}>
+      <h1>Kabir Sagar</h1>
+      <p>Software Developer, Educator at Geekster</p>
+      <a href="/">Linkedin</a>
+      &nbsp;
+      <a href="/">Github</a>
+      <hr />
+      <h1>Experience</h1>
+      <h4>Software developer at Amazon</h4>
+      <p>I work with amazon in transportation team, some more description Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, ullam! Maxime officiis consequuntur libero eaque reprehenderit quisquam nam ab vitae totam id commodi laborum enim, sint ea odit quasi soluta!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, soluta laboriosam? Quaerat sequi eveniet libero labore id itaque, aperiam ipsum hic asperiores repellat officia ducimus expedita ea, eius voluptate dolorum?</p>
+      <hr />
+      <h3>Educator at Geekster</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error corporis incidunt nesciunt perspiciatis, illo ipsam mollitia veniam fugit sapiente perferendis eius accusantium repellendus magni dolore qui consectetur numquam consequuntur, provident rerum deserunt alias voluptas nostrum esse adipisci? Ratione deserunt harum odit veniam impedit cumque, nulla error eos consequatur. Sunt molestiae, voluptatem veniam atque eos accusamus natus! Optio rerum voluptatem quo beatae fugiat provident magni vel voluptatum at recusandae sapiente incidunt odio debitis amet ad veniam quos atque ullam, corrupti illum accusamus laboriosam minima doloribus doloremque? Facilis ab ducimus ipsum, distinctio amet aliquid facere ad quam assumenda quod? Provident, odit veritatis sit dolor nemo aut nesciunt nulla cum corporis in facilis maiores nihil et dicta soluta hic aliquid, placeat harum nostrum! Labore ipsum velit necessitatibus illum a soluta sunt magni perspiciatis doloremque? Consectetur accusamus accusantium illum cumque molestias voluptatem obcaecati doloremque vero et modi quis, eligendi ullam placeat ipsa quisquam necessitatibus?</p>
+      <hr />
+      <h1>Skills</h1>
+      <ul>
+        <li>java</li>
+        <li>javaScript</li>
+        <li>testing</li>
+        <li>react</li>
+        <li>system designing</li>
+      </ul>
+      <hr />
+      <h1>Hobbies</h1>
+      <ul>
+        <li>chess</li>
+        <li>cricket</li>
+        <li>reading books</li>
+        <li>Teaching</li>
+      </ul>
+   </div>
+  )
+}
+
+export default Comp3
